c2537062c1db3df8194dbac18179c71651a73ce4
imthokozani <tnyawose25@gmail.com>
2026-02-12 14:38:39 +0200

    changes

--- WEBSITE/__init__.py
+++ WEBSITE/__init__.py
@@ -1,14 +1,18 @@
 from os import path
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "the application"
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    #app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(BASE_DIR, DB_NAME)}"
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
     from .views import views
